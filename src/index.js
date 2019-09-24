d823496a976dd422b894273aeff652b7170465d3
Alex <alekseys.workemail@gmail.com>
2019-09-24 21:40:19 +0300

    Added task with all comments

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 deleteNotUsedInArray = (str, bracketsConfig) => {
   let temp = bracketsConfig.map(x => x.join(""));
+  if (str.length % 2 != 0) {
+    return false;
+  }
 
+  let con =() => temp.some(x=> str.indexOf(x)>-1);
+  let i = 0;
+  while (str.length > 0  && con()) {
+    // && temp.length > 0
 
-  while (str.length > 0 && temp.length > 0) {
-    let i = 0;
     while (str.indexOf(temp[i]) > -1) {
+      // console.log(temp[i]);
+      let indexsrc = str.indexOf(temp[i]);
+      // str.splice(indexsrc,2);
+      // str = str.replace(new RegExp(temp[i], 'g'), "");
       str = str.replace(temp[i].toString(), "");
+      // str = str.split(temp[i].toString()).join("");
+      // let index = temp.indexOf(temp[i]);
+      // temp.splice(index,1);
+
     }
-    let index = temp.indexOf(temp[i]);
-    temp = temp.splice(1, index);
-    ++i;
+    i>= temp.length ? i = 0 : i=i+1;
 
   }
-  return str.length == 0 && temp.length == 0;
+  let retval = str.length == 0;
+  return retval;
 }
 
-deleteNotUsedInArray('()', [['(', ')']]);
+// deleteNotUsedInArray('|()|(||)||', [['(', ')'], ['|', '|']]);
 module.exports = function check(str, bracketsConfig) {
   // your solution
   return deleteNotUsedInArray(str, bracketsConfig);
 }
+
+
